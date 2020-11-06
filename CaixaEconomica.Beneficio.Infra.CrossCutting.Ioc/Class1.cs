b0181e1b9a9de6878e7bbdc54c2c35910d3eb800
Leivas Nascimento Dos Santos <lnsantos2@latam.stefanini.com>
2020-11-06 09:22:09 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEconomica.Beneficio.Infra.CrossCutting.Ioc/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CaixaEconomica.Beneficio.Infra.CrossCutting.Ioc
+{
+    public class Class1
+    {
+        //Ioc - inversão de controle. Será configurado nesse projeto
+    }
+}
